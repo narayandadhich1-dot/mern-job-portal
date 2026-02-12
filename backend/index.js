b18e9041afca7ee1c13a86f29932147925d6b43e
Narayan Dadhich <narayandadhich1@gmail.com>
2026-02-12 17:17:31 +0530

    Added our frontend URL

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "http://localhost:5173", 
+  origin: ["http://localhost:5173", "https://mern-job-portal-sepia.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
