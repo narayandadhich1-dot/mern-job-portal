dc13e32c73302e39ac59728709feea61ad81fb34
Narayan Dadhich <narayandadhich1@gmail.com>
2026-02-23 14:11:59 +0530

    add

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -1,30 +1,29 @@
 import { setAllJobs } from "@/redux/jobslice";
-import store from "@/redux/store";
 import { JOB_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 const useGetAllJobs = () => {
-  const disptach = useDispatch();
+  const dispatch = useDispatch();
   const { searchedQuery } = useSelector((store) => store.job);
-
 
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
         const res = await axios.get(
           `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
-          { withCredentials: true },
+          { withCredentials: true }
         );
         if (res.data.success) {
-          disptach(setAllJobs(res.data.jobs));
+          dispatch(setAllJobs(res.data.jobs));
         }
       } catch (error) {
         console.log(error);
       }
     };
     fetchAllJobs();
-  }, [searchedQuery]);
+  }, [searchedQuery, dispatch]); 
 };
+
 export default useGetAllJobs;
